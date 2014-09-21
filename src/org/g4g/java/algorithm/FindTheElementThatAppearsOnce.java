601cf847469478a8a7281c7c63af749f73862a28
stb6688 <mr.tshen@gmail.com>
2014-09-21 14:10:08 -0400

    add multiple solutions

--- src/org/g4g/java/algorithm/FindTheElementThatAppearsOnce.java
+++ src/org/g4g/java/algorithm/FindTheElementThatAppearsOnce.java
@@ -1,9 +1,14 @@
 package org.g4g.java.algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Question: 
  *
-Given an array where every element occurs three times, except one element which occurs only once. Find the element that occurs once. Expected time complexity is O(n) and O(1) extra space.
+Given an array where every element occurs three times, 
+except one element which occurs only once. Find the element that occurs once. 
+Expected time complexity is O(n) and O(1) extra space.
 Examples:
 
 Input: arr[] = {12, 1, 12, 3, 12, 1, 1, 2, 3, 3}
@@ -31,16 +36,16 @@ public class FindTheElementThatAppearsOnce {
  * 	if bit=0 --> sum=0, module=0;
  * So: each resulted bit is essentially equal to the bit on target element!
  * 
- * find the largest & smallest element, calculate the longest digits --> len
- * for n: 0 to len
- * 		mask = 1 << n
- * 		then element & mask and sum up; result module with 3.
- * add up the result from each iteration, that is the target element.
  * @author admin
  *
  */
 public class FindTheElementThatAppearsOnce {
 	
+	// find the largest & smallest element, calculate the longest digits --> len
+	// for n: 0 to len
+	//		mask = 1 << n
+	// 		then element & mask and sum up; result module with 3.
+	// add up the result from each iteration, that is the target element.
 	public int findInt(int[] arr) {
 		int result = 0;
 		for (int n = 0; n < 32; n++) {
@@ -56,17 +61,41 @@ public class FindTheElementThatAppearsOnce {
 		
 		return result;
 	}
+	
+	// SOLUTION I: use HashMap, which gives linear performance 
+	// and linear space.
+	// key is element, value is occurrences;
+	// if an element's count reaches 3, remove the element.
+	// in the end, only 1 element should remain in map.
+//	public int findInt(int[] arr) {
+//		Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
+//		for (int i : arr) {
+//			Integer count = countMap.get(i);
+//			if (count == null)
+//				count = 1;
+//			else
+//				count++;
+//			if (count < 3)
+//				countMap.put(i, count);
+//			else
+//				countMap.remove(i);
+//		}
+//		return countMap.keySet().iterator().next();
+//	}
 
 	public static void main(String[] args) {
 		FindTheElementThatAppearsOnce instance = new FindTheElementThatAppearsOnce();
 		int result;
 		
-		// result = 2
+		// first test
+		// result=2
 //		int[] arr = new int[]{12, 1, 12, 3, 12, 1, 1, 2, 3, 3};
 //		result = instance.findInt(arr);
 //		System.out.println(result);
 		
-		int[] arr = new int[]{-12, -1, -12, -3, -12, -1, -1, -2, -3, -3};
+		// test with negative number, where leftmost bit is 1.
+		// result=-2
+		int[] arr = new int[]{-12, -1, -12, 3, -12, -1, -1, -2, 3, 3};
 		result = instance.findInt(arr);
 		System.out.println(result);
 	}
